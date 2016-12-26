cf514eeabc35310c04a3739cb4c41c32bb9ed4af
Cow <cowcoa@163.com>
2016-12-26 09:58:34 +0800

    调整Linux下生成工程的脚本,在克隆cos-cpp-sdk-v4之前先将其删除.

--- Generate.sh
+++ Generate.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+rm -rf ./cos-cpp-sdk-v4
 git clone https://github.com/tencentyun/cos-cpp-sdk-v4.git
 
 rm -rf ./Build
 mkdir -p ./Build/Bin
 cp ./cosconfig.json ./Build/Bin
+
 cd Build
 cmake -DBOOST_ROOT=/usr/local/bin ..
