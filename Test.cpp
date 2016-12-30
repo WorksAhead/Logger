031c64ccdbb2fe9ffd6218d32a6c5b3ef24d188e
Cow <cowcoa@163.com>
2016-12-30 11:28:20 +0800

    添加command line解析和Linux下daemon进程的处理.

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,9 @@
 #include "QCOSLogger.h"
 #include "BoostLogWrapper.h"
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
 using namespace QCOS;
 
 class test
@@ -12,11 +15,87 @@ public:
     }
 };
 
-
-
-int main()
+int Daemonize(const char* name)
 {
-    InitBoostLog("COSTest.log");
+#if defined (_WIN32)
+    // Nothing to do
+#elif defined (__linux__) || defined (__FreeBSD__)
+    pid_t child;
+    FILE* f;
+    char pidFile[80];
+
+    child = fork();
+    if (!child)
+    {
+        child = fork();
+        if (child) exit(0);
+    }
+    else
+    {
+        // The master process collects the zombie 
+        // process of forking the child daemon and exits 
+        //#if !defined (__FreeBSD__)
+        //                waitpid(child, NULL, 0);
+        //                return 1;
+        //#else
+        exit(0);
+        //#endif
+    }
+
+    // Get rid of controlling terminal.
+    setsid();
+
+    // Have to close the original standard I/O 
+    // streams and create our own standard I/O streams,
+    // otherwise the parment process cannot exit cleanly 
+    umask(0);
+    close(0);
+    close(1);
+    close(2);
+    open("/dev/null", O_RDWR, 0);
+    dup2(0, 1);
+    dup2(0, 2);
+
+    snprintf(pidFile, sizeof(pidFile), "/var/run/%s.pid", name);
+    f = fopen(pidFile, "w");
+    if (!f)
+    {
+        fprintf(stderr, "failed to open %s: %s", pidFile, strerror(errno));
+    }
+    else
+    {
+        fprintf(f, "%u\n", getpid());
+        fclose(f);
+}
+#endif
+    return 0;
+}
+
+int main(int ac, char* av[])
+{
+    bool runAsDaemon = false;
+
+#if defined (__linux__)
+    po::options_description desc("Generic options");
+    desc.add_options()
+        ("daemon,d", "Running as a daemon process.")
+        ;
+
+    po::options_description cmdline_options;
+    cmdline_options.add(desc);
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(ac, av, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("daemon"))
+    {
+        runAsDaemon = true;
+        Daemonize("QCOSTest");
+    }
+#endif
+
+    InitBoostLog("COSTest.log", runAsDaemon);
 
     QCOSLogger logger;
 
